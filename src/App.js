3d03864a1421a4621f72f919837a8572afb9fc30
Vaishvi Gupta <111447942+vaishvii@users.noreply.github.com>
2023-08-26 15:55:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';//
+
+import './App.css';
+import { SearchPage } from './pages/SearchPage';
+
+function App() {
+  return (
+   <SearchPage/>
+  );
+}
+
+export default App;
